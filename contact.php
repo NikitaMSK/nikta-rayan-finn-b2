5743bc902f1b49f8200dcda66eaaeb5a489ebec0
NikitaMSK <nikiperetz10@gmail.com>
2026-04-15 18:24:31 +0200

    Een contactpagina toevoegen
    Een contactformulier en CSS toevoegen

--- contact.php
+++ contact.php
@@ -11,26 +11,47 @@ if (!isset($_SESSION['user_id'])) {
 
 <head>
     <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="css/contact.css">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <meta name="viewport">
     <title>Contact pagina</title>
-    <?php require_once 'head.php'; ?>
 </head>
 
 <body>
     <header class="task-header">
-        <div class="task-brand">
-            <div class="logo">Developerland</div>
-            <nav>
-                <p>Welkom: <?php echo $_SESSION['username'] ?></p>
+        <div class="form-group">
+            <form action="">
+                <div class="logo">Developerland</div>
+                <nav>
+                    <p>Welkom: <?php echo $_SESSION['username'] ?></p>
 
-                <a href="../index.php">Home</a>
-                <a href="create.php" class="btn-action">Nieuwe melding</a>
-                <a href="#">Contact</a>
-                <a href="../logout.php">Logout</a>
-            </nav>
+                    <a href="../index.php">Home</a>
+                    <a href="../create.php" class="btn-action">New Task</a>
+                    <a href="../logout.php">Log out</a>
+                </nav>
+            </form>
         </div>
     </header>
+    <main>
+        <form class="contact-form" onsubmit="return false;">
+            <div class="input-group">
+                <label for="name">Name</label>
+                <input type="text" id="name" >
+            </div>
+
+            <div class="input-group">
+                <label for="email">Email</label>
+                <input type="email" id="email">
+            </div>
+
+            <div class="input-group">
+                <label for="message">Message</label>
+                <textarea id="message" ></textarea>
+            </div>
+
+            <button type="submit" class="submit-btn">Sumbit</button>
+        </form>
+    </main>
 </body>
 
 </html>
